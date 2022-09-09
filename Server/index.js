77a5e9f45e62fb38f812868f9c168d151ae5a70b
tmahere <80914897+tmahere@users.noreply.github.com>
2022-09-09 01:46:06 -0400

    Basic Server configurations
    Connected server to MongoDB atlas as well as connected it to a port

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cors from 'corse';
+import cors from 'cors';
+
+import MONGO_URI from './keys.js';
+
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended:true}));
+app.use(bodyParser.urlencoded({"limit": "30mb", extended:true}));
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGO_URI)
+    .then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log("error connecting to database", err))
---
+++ Server/keys.js
@@ -0,0 +1,3 @@
+const MONGO_URI = "mongodb+srv://tmahere:tmahere123@cluster0.tyzlllq.mongodb.net/?retryWrites=true&w=majority"
+
+export default MONGO_URI;
