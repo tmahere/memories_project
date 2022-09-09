77e6c07ea81ba02ff4ef7d17dc674820527961bd
tmahere <80914897+tmahere@users.noreply.github.com>
2022-09-09 02:23:41 -0400

    Worked on backend scalability
    implemtned MVC folder structure -- created controller folder to hold all handler logic -- abstracted from route folder which now just holds all the routes -- created a get and post route

--- Server/routes/posts.js
+++ Server/routes/posts.js
@@ -1,9 +1,12 @@
 import express from 'express';
 
+import {getPosts} from '../controllers/posts.js';
+import {createPost} from '../controllers/posts.js';
+
 const router = express.Router();
 
-router.get('/', (req, res) =>{
-    res.send("THIS WORKS");
-})
+
+router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
