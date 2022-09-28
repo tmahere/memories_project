15caa6fedf2653665aa99c319af60350f703497e
tmahere <80914897+tmahere@users.noreply.github.com>
2022-09-28 19:10:06 -0400

    final push
    This memories website has basic functionality. You can post a memory with descriptors and hashtags. That will then be displayed to you. You can like or delete a post as you  wish. Minimal functionality. for learning purposes only.

--- Client/src/reducers/posts.js
+++ Client/src/reducers/posts.js
@@ -3,15 +3,14 @@
 export default(posts = [], action) =>{
     switch (action.type){
         case 'DELETE':
-            return posts.filter((post) => post._id !== action.payload._id)
+            return posts.filter((post) => post._id !== action.payload._id);
         case 'UPDATE':
         case 'LIKE':
-            return posts.map((post) => post._id == action.payload._id ? action.payload : post)
+            return posts.map((post) => post._id == action.payload._id ? action.payload : post);
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
             return [...posts, action.payload];
-
         default:
             return posts;
     }
